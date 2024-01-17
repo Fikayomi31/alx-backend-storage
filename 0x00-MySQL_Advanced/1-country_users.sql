d3bfe51d53cb1de07f542ee94df71bed3d86dae0
Fikayomi31 <fikayoogundijo@gmail.com>
2024-01-17 23:38:55 +0000

    in and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Createa table users with id, email, name, country
+
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
