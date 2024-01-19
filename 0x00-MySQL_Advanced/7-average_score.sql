8ebc3f90996a355e663d4bc0dfb91bbcc273523b
Fikayomi31 <fikayoogundijo@gmail.com>
2024-01-19 23:03:32 +0000

    average

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -5,7 +5,7 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 	DECLARE avgscore FLOAT;
-	SELECT AVG(score) AS avgscore FROM corrections AS c WHERE c.user_id=user_id);
+	SELECT AVG(score) AS avgscore FROM corrections AS c WHERE c.user_id=user_id;
 	UPDATE users SET average_score = avgscore WHERE id = user_id;
 END;
 //
