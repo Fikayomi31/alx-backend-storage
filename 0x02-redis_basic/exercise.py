9445bef7a04893a15dfcf2cba26d569483c8e7eb
Fikayomi31 <fikayoogundijo@gmail.com>
2024-03-23 00:26:58 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,7 +14,26 @@ class Cache:
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Store a random set of data"""
+        """Storing a random set of data
+        Arg:
+            data: can be either str, bytes, int, float
+        """
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[
+                Callable] = None) -> Union[str, bytes, int, float]:
+        """Converting data back to it desired format"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Get a string"""
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> int:
+        """Get a int"""
+        return self.get(key, int)
